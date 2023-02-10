2212e02604e6ebad30d0eae1b317935cc159e75d
wighawag <wighawag@gmail.com>
2023-02-10 22:44:06 +0000

    change path to reflect trailing slash settings

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,8 +19,8 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/about/' ? 'page' : undefined}>
+				<a href="/about/">About</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
